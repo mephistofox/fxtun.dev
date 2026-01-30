8ecf91a544e6606201b846910633e9bc467d80da
Mephisto Fox <jakyll.fox@gmail.com>
2026-01-30 16:22:02 +0300

    feat(inspect): add CapturedExchange data model

---
+++ internal/inspect/exchange.go
@@ -0,0 +1,51 @@
+package inspect
+
+import (
+	"net/http"
+	"time"
+)
+
+const MaxBodySize = 256 * 1024
+
+type CapturedExchange struct {
+	ID        string        `json:"id"`
+	TunnelID  string        `json:"tunnel_id"`
+	Timestamp time.Time     `json:"timestamp"`
+	Duration  time.Duration `json:"duration_ns"`
+
+	Method          string      `json:"method"`
+	Path            string      `json:"path"`
+	Host            string      `json:"host"`
+	RequestHeaders  http.Header `json:"request_headers"`
+	RequestBody     []byte      `json:"request_body,omitempty"`
+	RequestBodySize int64       `json:"request_body_size"`
+	RemoteAddr      string      `json:"remote_addr"`
+
+	StatusCode       int         `json:"status_code"`
+	ResponseHeaders  http.Header `json:"response_headers"`
+	ResponseBody     []byte      `json:"response_body,omitempty"`
+	ResponseBodySize int64       `json:"response_body_size"`
+}
+
+type ExchangeSummary struct {
+	ID               string        `json:"id"`
+	TunnelID         string        `json:"tunnel_id"`
+	Timestamp        time.Time     `json:"timestamp"`
+	Duration         time.Duration `json:"duration_ns"`
+	Method           string        `json:"method"`
+	Path             string        `json:"path"`
+	Host             string        `json:"host"`
+	StatusCode       int           `json:"status_code"`
+	RequestBodySize  int64         `json:"request_body_size"`
+	ResponseBodySize int64         `json:"response_body_size"`
+	RemoteAddr       string        `json:"remote_addr"`
+}
+
+func (e *CapturedExchange) Summary() ExchangeSummary {
+	return ExchangeSummary{
+		ID: e.ID, TunnelID: e.TunnelID, Timestamp: e.Timestamp, Duration: e.Duration,
+		Method: e.Method, Path: e.Path, Host: e.Host, StatusCode: e.StatusCode,
+		RequestBodySize: e.RequestBodySize, ResponseBodySize: e.ResponseBodySize,
+		RemoteAddr: e.RemoteAddr,
+	}
+}
