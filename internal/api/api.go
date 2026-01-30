cd127aa168fcd5bae135f56a7c1e64493bd2ad58
Mephisto Fox <jakyll.fox@gmail.com>
2026-01-30 22:24:13 +0300

    fix(api): add max password length, log JSON encoding errors

--- internal/api/api.go
+++ internal/api/api.go
@@ -380,7 +380,9 @@ func (s *Server) respondJSON(w http.ResponseWriter, status int, data interface{}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	if data != nil {
-		json.NewEncoder(w).Encode(data)
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			s.log.Error().Err(err).Msg("failed to encode JSON response")
+		}
 	}
 }
 
