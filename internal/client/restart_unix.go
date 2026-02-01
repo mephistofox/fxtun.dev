fec4fc9bb647ab2003a31758ef692a871e467120
Mephisto Fox <jakyll.fox@gmail.com>
2026-02-01 22:55:46 +0300

    fix(lint): suppress G204 gosec warning for syscall.Exec in self-restart

--- internal/client/restart_unix.go
+++ internal/client/restart_unix.go
@@ -9,7 +9,7 @@ import (
 )
 
 func restartProcess(execPath string) error {
-	if err := syscall.Exec(execPath, os.Args, os.Environ()); err != nil {
+	if err := syscall.Exec(execPath, os.Args, os.Environ()); err != nil { //nolint:gosec // execPath is our own binary from os.Executable()
 		return fmt.Errorf("restart: %w", err)
 	}
 	return nil // unreachable
