482b8d3ec2ddf7e094a8bdd088fac660e4758435
Mephisto Fox <jakyll.fox@gmail.com>
2026-02-12 00:09:06 +0300

    chore: rewrite go module path to fxtun.dev during sync

--- scripts/sync-public.sh
+++ scripts/sync-public.sh
@@ -21,6 +21,12 @@ git filter-repo \
   --path-glob 'web/public/*.txt' \
   --force
 
+# Rewrite Go module path for public repo
+OLD_MODULE="github.com/mephistofox/fxtunnel"
+NEW_MODULE="github.com/mephistofox/fxtun.dev"
+find . -name '*.go' -o -name 'go.mod' | xargs sed -i "s|${OLD_MODULE}|${NEW_MODULE}|g"
+git add -A && git commit -m "chore: rewrite module path to ${NEW_MODULE}" --allow-empty
+
 # Create repo if it doesn't exist
 gh repo view "$PUBLIC_REPO" &>/dev/null || \
   gh repo create "$PUBLIC_REPO" --public --description "$DESCRIPTION"
